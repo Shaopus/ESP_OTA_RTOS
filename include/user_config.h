637c593e2e5d3ce9ef332679041a25c339981301
Shaopus <shaopus@163.com>
2016-09-09 22:47:54 +0800

    OTA
    OTA For ESP_RTOS.Use HFS Server

---
+++ include/user_config.h
@@ -0,0 +1,13 @@
+#ifndef __USER_CONFIG_H__
+#define __USER_CONFIG_H__
+
+#define SERVER_DEMO 0
+#define CLIENT_DEMO 1
+#define UDP_DEMO 0
+#define ECHO_DEMO	1
+
+#define DEMO_LENGTH 2048
+#define DEMO_PORT 7007
+
+#endif
+
